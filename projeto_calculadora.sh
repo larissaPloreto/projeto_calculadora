c7a54ce8508efce4c2430260691ccd00c6bd3bd1
larissaPloreto <larissaploreto@gmail.com>
2024-10-10 13:58:20 -0300

    Create projeto_calculadora.sh
    Adiciona arquivo executável

---
+++ projeto_calculadora.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Calculadora
+
+#Menu
+
+# As Variáveis
+
+echo "Digite o primeiro número: "
+read num1
+echo "Digite o segundo número: "
+read num2
+
+echo "Escolha a operação (+,-,\*,/):"
+read operacao
+
+#Operações Básicas
+case $operacao in
+    +)resultado=$(echo "$num1 + $num2" | bc);;
+    -)resultado=$(echo "$num1 - $num2" | bc);;
+    \*)resultado=$(echo "$num1 * $num2" | bc);;
+    /)resultado=$(echo "scale=2; $num1 / $num2" | bc);;
+    *)echo "Operação Inválida"; exit 1;;
+esac
+
+echo "Resultado: $resultado"
